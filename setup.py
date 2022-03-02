a318f0c4a8cf4266a51f717270d325e04f4077e0
Emmanuel I. Obi <withtwoemms@gmail.com>
2022-03-02 12:47:38 -0600

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from os import environ as envvars
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='recruitment',
+    description='a lib for sourcing actionpacked entities capable of getting the job done',
+    long_description=Path(__file__).absolute().parent.joinpath('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    setup_requires=[
+        'setuptools_scm==5.0.1'
+    ],
+    use_scm_version={'local_scheme': 'no-local-version'} if envvars.get('LOCAL_VERSION_SCHEME') else True,
+    packages=find_packages(exclude=['tests']),
+    author='Emmanuel I. Obi',
+    maintainer='Emmanuel I. Obi',
+    maintainer_email='withtwoemms@gmail.com',
+    url='https://github.com/withtwoemms/actionpack',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
+
