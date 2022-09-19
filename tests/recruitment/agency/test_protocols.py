ced31f3a02d7f5dceb1f52d68244adacbfbc4083
Emmanuel I. Obi <withtwoemms@gmail.com>
2022-09-19 11:39:52 -0500

    updates HasContingency protocol tests to include Agents and Consumers

--- tests/recruitment/agency/test_protocols.py
+++ tests/recruitment/agency/test_protocols.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 from unittest.mock import patch
 
+from recruitment.agency import Agent
 from recruitment.agency import Config
+from recruitment.agency import Consumer
 from recruitment.agency import Publisher
 from recruitment.agency.protocols import HasContingency
 
@@ -11,6 +13,12 @@ from tests.recruitment.agency import acceptable_broker_names
 class HasContingencyTest(TestCase):
 
     @patch('boto3.client')
-    def test_can_follow_protocol(self, mock_boto_client):
+    def test_fully_implemented_entities_follow_protol(self, mock_boto_client):
         broker_name = acceptable_broker_names[0]
-        self.assertIsInstance(Publisher(Config(broker_name)), HasContingency)
+        config = Config(broker_name)
+        self.assertIsInstance(Agent(config), HasContingency)
+        self.assertIsInstance(Publisher(config), HasContingency)
+
+    @patch('boto3.client')
+    def test_partial_entity_violates_protocol(self, mock_boto_client):
+        self.assertNotIsInstance(Consumer(), HasContingency)
