5aa542c62023d3227fcff962c12fdfbfdad1d0e7
Emmanuel I. Obi <withtwoemms@gmail.com>
2022-10-29 23:53:21 -0500

    protocol test suite mocks boto client

--- tests/recruitment/agency/test_protocols.py
+++ tests/recruitment/agency/test_protocols.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 from recruitment.agency import Config
 from recruitment.agency import Commlink
@@ -6,14 +7,23 @@ from recruitment.agency import Contingency
 from recruitment.agency import Coordinator
 from recruitment.agency.protocols import HasContingency
 
-from tests.recruitment.agency import acceptable_broker_names
+from tests.recruitment.agency import client
+from tests.recruitment.agency import fake_credentials
 from tests.recruitment.agency import retry_policy_provider
 
 
 class HasContingencyTest(TestCase):
 
-    broker_name = acceptable_broker_names[0]
-    commlink = Commlink(Config(broker_name))
+    def setUp(self):
+        broker_name = 'sns'
+        region = 'us-east-1'
+
+        patcher = patch('boto3.client')
+        self.addCleanup(patcher.stop)
+        self.mock_boto_client = patcher.start()
+        self.mock_boto_client.return_value = client(broker_name, region)
+
+        self.commlink = Commlink(Config(broker_name, **fake_credentials))
 
     def test_arbitrary_class_can_adhere_to_HasContingency_protocol(self):
         class Conformer:
