76668bc16defb4efb99f099032fb1c0108554662
Emmanuel Obi <87988251+manualautomaton@users.noreply.github.com>
2022-11-06 17:36:30 -0600

    recruitment#48: "supports temporary AWS credentials" (#49)
    * updates ConfigTest suite
    
    * improves test coverage
    
    * Config object supports session_token param

--- tests/recruitment/agency/test_config.py
+++ tests/recruitment/agency/test_config.py
@@ -1,11 +1,11 @@
 from os import environ as envvars
-from dataclasses import asdict
 from textwrap import dedent
 from unittest import TestCase
 from unittest.mock import patch
 
 from tests.recruitment.agency import acceptable_broker_names
 from tests.recruitment.agency import fake_credentials
+from tests.recruitment.agency import fake_credentials_with_session_token
 from recruitment.agency import Config
 
 
@@ -36,9 +36,16 @@ class ConfigTest(TestCase):
     def test_can_instantiate(self):
         for name in acceptable_broker_names:
             config = Config(name, **fake_credentials)
-            config_dict = asdict(config)
+            config_dict = dict(config)
             config_dict.pop('service_name')
             self.assertDictEqual(config_dict, fake_credentials)
+
+    def test_can_instantiate_with_session_token(self):
+        for name in acceptable_broker_names:
+            config = Config(name, **fake_credentials_with_session_token)
+            config_dict = dict(config)
+            config_dict.pop('service_name')
+            self.assertDictEqual(config_dict, fake_credentials_with_session_token)
 
     def test_can_serialize(self):
         config = Config(self.broker_name, **fake_credentials)
@@ -105,6 +112,84 @@ class ConfigTest(TestCase):
         self.assertEqual(config.access_key_id, fake_credentials['access_key_id'])
         self.assertEqual(config.secret_access_key, fake_credentials['secret_access_key'])
         self.assertEqual(config.endpoint_url, fake_credentials['endpoint_url'])
+
+    @patch.dict(
+        envvars,
+        {
+            'AWS_SERVICE_NAME': broker_name,
+            'AWS_REGION_NAME': fake_credentials_with_session_token['region_name'],
+            'AWS_ACCESS_KEY_ID': fake_credentials_with_session_token['access_key_id'],
+            'AWS_SECRET_ACCESS_KEY': fake_credentials_with_session_token['secret_access_key'],
+            'AWS_SESSION_TOKEN': fake_credentials_with_session_token['session_token'],
+            'AWS_ENDPOINT_URL': fake_credentials_with_session_token['endpoint_url'],
+        },
+        clear=True
+    )
+    def test_can_supplement_Config_instance_from_environment_with_session_token(self):
+        some_service_name = 'kinesis'
+        self.assertNotEqual(self.broker_name, some_service_name)
+        config = Config(service_name=some_service_name)
+        self.assertEqual(config.service_name, some_service_name)
+        self.assertEqual(config.region_name, None)
+        self.assertEqual(config.access_key_id, None)
+        self.assertEqual(config.secret_access_key, None)
+        self.assertEqual(config.session_token, None)
+        self.assertEqual(config.endpoint_url, None)
+        config = config.supplement('env')
+        self.assertEqual(config.service_name, some_service_name)
+        self.assertEqual(config.region_name, fake_credentials_with_session_token['region_name'])
+        self.assertEqual(config.access_key_id, fake_credentials_with_session_token['access_key_id'])
+        self.assertEqual(config.secret_access_key, fake_credentials_with_session_token['secret_access_key'])
+        self.assertEqual(config.session_token, fake_credentials_with_session_token['session_token'])
+        self.assertEqual(config.endpoint_url, fake_credentials_with_session_token['endpoint_url'])
+
+    @patch.dict(
+        envvars,
+        {
+            'AWS_SERVICE_NAME': broker_name,
+            'AWS_SESSION_TOKEN': '',
+        },
+        clear=True
+    )
+    def test_session_token_attr_absent_if_envvar_is_empty(self):
+        config = Config.fromenv()
+        with self.assertRaises(AttributeError):
+            config.session_token
+
+    @patch.dict(
+        envvars,
+        {
+            'AWS_SERVICE_NAME': broker_name,
+            'AWS_SESSION_TOKEN': fake_credentials_with_session_token['session_token'],
+        },
+        clear=True
+    )
+
+    def test_session_token_attr_present_if_envvar_has_value(self):
+        config = Config.fromenv()
+        self.assertEqual(config.session_token, fake_credentials_with_session_token['session_token'])
+
+    @patch.dict(
+        envvars,
+        {
+            'AWS_SERVICE_NAME': broker_name,
+            'AWS_REGION_NAME': fake_credentials_with_session_token['region_name'],
+            'AWS_ACCESS_KEY_ID': fake_credentials_with_session_token['access_key_id'],
+            'AWS_SECRET_ACCESS_KEY': fake_credentials_with_session_token['secret_access_key'],
+            'AWS_SESSION_TOKEN': fake_credentials_with_session_token['session_token'],
+            'AWS_ENDPOINT_URL': fake_credentials_with_session_token['endpoint_url'],
+        },
+        clear=True
+    )
+    def test___repr__(self):
+        # sensitive to env ordering
+        obj_repr = ('Config(service_name=logs, '
+                    'region_name=somewhere-in-the-world, '
+                    'access_key_id=s3curityBadge!, '
+                    'secret_access_key=p@ssw0rd!, '
+                    'endpoint_url=some-computer.com, '
+                    'session_token=jamSesh3000!)')
+        self.assertEqual(obj_repr, repr(Config.fromenv()))
 
     def test_cannot_instantiate_without_service_name(self):
         with self.assertRaises(Config.AttributeDeclaredIncorrectly):
